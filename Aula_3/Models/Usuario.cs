2c0488f211b9627b21f7dcd6acf81c45adf8440d
Cássio <bindaco77@gmail.com>
2025-05-27 22:07:50 -0300

    Aula_03

--- Aula_3/Models/Usuario.cs
+++ Aula_3/Models/Usuario.cs
@@ -2,7 +2,7 @@ namespace Aula_3.Models
 {
     public class Usuario
     {
-        public int Id { get; set; }
+        public int IdUsuario { get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
 
@@ -19,13 +19,40 @@ namespace Aula_3.Models
         static Usuario()
         {
             // Adicionando alguns usuários de exemplo
-           Usuario.listagem.Add(new Usuario { Id = 1, Nome = "João", Email = "joao@email.com" });
-           Usuario.listagem.Add(new Usuario { Id = 2, Nome = "Maria", Email = "maria@email.com" });
-           Usuario.listagem.Add(new Usuario { Id = 3, Nome = "Carlos", Email = "carlos@email.com" });
-           Usuario.listagem.Add(new Usuario { Id = 4, Nome = "Ana", Email = "ana@email.com" });
-           Usuario.listagem.Add(new Usuario { Id = 5, Nome = "Pedro", Email = "pedro@email.com" });
-           Usuario.listagem.Add(new Usuario { Id = 6, Nome = "Fernanda", Email = "fernanda@email.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 1, Nome = "João", Email = "joao@email.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 2, Nome = "Maria", Email = "maria@email.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 3, Nome = "Carlos", Email = "carlos@email.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 4, Nome = "Ana", Email = "ana@email.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 5, Nome = "Pedro", Email = "pedro@email.com" });
+            Usuario.listagem.Add(new Usuario { IdUsuario = 6, Nome = "Fernanda", Email = "fernanda@email.com" });
         }
+
+        public static void Salvar(Usuario usuario)
+        {
+            var usuarioExistente = Usuario.listagem.Find(u => u.IdUsuario == usuario.IdUsuario);
+            if (usuarioExistente != null)
+            {
+                usuarioExistente.Nome = usuario.Nome;
+                usuarioExistente.Email = usuario.Email;
+            }
+            else
+            {
+                int maiorId = Usuario.Listagem.Max(u => u.IdUsuario);
+                usuario.IdUsuario = maiorId + 1;
+                Usuario.listagem.Add(usuario);
+            }
+        }
+
+        public static void Excluir(int idUsuario)
+        {
+            var usuarioExistente = Usuario.listagem.Find(u => u.IdUsuario == idUsuario);
+            if (usuarioExistente != null)
+            {
+                Usuario.listagem.Remove(usuarioExistente);
+            }
+            
+        }
+
     
     }
 }
--- Aula_3/obj/Debug/net8.0/Aula_3.AssemblyInfo.cs
+++ Aula_3/obj/Debug/net8.0/Aula_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32b8aa890f8d24269b0449add0f5d7607dcb838")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b2c3761db59438fb188f25ab05ee3c1a0e2f76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
