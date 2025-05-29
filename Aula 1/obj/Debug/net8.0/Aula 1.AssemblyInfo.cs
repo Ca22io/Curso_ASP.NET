588585b519d84adc91bcf9ace9f570ad8ce82add
Cássio Zanon Bindaco <bindaco77@gmail.com>
2025-05-29 15:06:32 -0300

    Aula_1_Concluida

--- Aula 1/obj/Debug/net8.0/Aula 1.AssemblyInfo.cs
+++ Aula 1/obj/Debug/net8.0/Aula 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20cb7973e33065f68c3f898965fe272b1f39e1e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e1927ac98543c6d01bc082952a6eb5cfbbda5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
