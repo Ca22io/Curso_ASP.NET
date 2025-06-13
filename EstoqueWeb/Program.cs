45e05fe6b6d72767e269a9c775e900360d71aa0c
Cássio <bindaco77@gmail.com>
2025-06-12 22:21:11 -0300

    Aula_15

---
+++ EstoqueWeb/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
